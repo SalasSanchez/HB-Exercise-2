c23aeff835788f6c09adc03a3f9760fa03d8c9a7
Hackbright Student <user@mini-005.(none)>
2014-02-05 18:06:56 -0800

    Only for positive integers

--- testing.py
+++ testing.py
@@ -1,8 +1,5 @@
 #import all functions from arithmetic file
 from arithmetic import *
-
-calcInput = raw_input("Parameters please.\n")
-calcList = calcInput.split(" ")
 operators = ["+", "-", "/", "*", "pow", "square", "cube", "mod"]
 opDictionary = {
     "+" : add ,
@@ -15,27 +12,28 @@ opDictionary = {
      "mod": mod
 }
 
-# this checks to see if the first in the list is an operator
-if calcList[0] not in operators:
-    print "Operator first please."
-else: # returns and saves the operator
-    calcOperator = calcList.pop(0)    
+while True:
+
+    calcInput = raw_input("Parameters please.\n")
+    calcList = calcInput.split(" ")
+
+    if calcInput != "q":
+        # this checks to see if the first in the list is an operator
+        if calcList[0] not in operators:
+            print "Operator first please."
+        else: # returns and saves the operator
+            calcOperator = calcList.pop(0)    
+    else:
+        break
 
 
 # check other items in list to see if they are int or float
-for i in calcList:
-
-    try:
-        int(i) 
-  
-    except ValueError:
-        try:
-            float(i)
-            print "It's all numbers"  
-        except ValueError:
+    for i in calcList:
+        if i.isdigit() == False:
             print "Give me number only please"
-            break
+       # if first item is operator
+        elif calcOperator in operators:
+            #then print the key paired function (in the dictionary) and pass it the parameters (the next items given by the user)
+            print opDictionary.get(calcOperator)(int(calcList[0]), int(calcList[1]))
 
-# if first item is operator, run matching operation
-if calcOperator in operators:
-    print opDictionary.get(calcOperator)(calcList[0], calcList[1])
+
